db78419cf98f25b9245eb180649265a21fcc39ca
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-05-21 15:29:06 -0400

    Fix range

--- feedforward.sh
+++ feedforward.sh
@@ -1,6 +1,6 @@
-for TRAIN_SIZE in {100000..900000..200000}
+for TRAIN_SIZE in {100000..700000..200000}
 do
-  for VOCAB_SIZE in {1000..10000..2000}
+  for VOCAB_SIZE in {1000..9000..2000}
   do
     TRAIN_SIZE=$TRAIN_SIZE VOCAB_SIZE=$VOCAB_SIZE msub -v feedforward.pbs
   done
