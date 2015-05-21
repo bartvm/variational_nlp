2586117853365192277135a9afd81a28eeeeb835
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-05-21 17:17:07 -0400

    only one command per job then..

--- feedforward.sh
+++ feedforward.sh
@@ -1,9 +1,8 @@
 export FUEL_DATA_PATH=$PWD
-for TRAIN_SIZE in {100000..700000..200000}
+for TRAIN_SIZE in {100000..700000..100000}
 do
-  for VOCAB_SIZE in {1000..9000..2000}
+  for VOCAB_SIZE in {1000..9000..1000}
   do
-    echo "Submitting..."
     TRAIN_SIZE=$TRAIN_SIZE VOCAB_SIZE=$VOCAB_SIZE msub -V feedforward.pbs
   done
 done
