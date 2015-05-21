02c2e1224073c9ccb617ac4148ce3c7e7ca0ad02
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-05-21 16:44:08 -0400

    set data path

--- feedforward.sh
+++ feedforward.sh
@@ -1,7 +1,9 @@
+export FUEL_DATA_PATH=$PWD
 for TRAIN_SIZE in {100000..700000..200000}
 do
   for VOCAB_SIZE in {1000..9000..2000}
   do
-    TRAIN_SIZE=$TRAIN_SIZE VOCAB_SIZE=$VOCAB_SIZE msub -v feedforward.pbs
+    echo "Submitting..."
+    TRAIN_SIZE=$TRAIN_SIZE VOCAB_SIZE=$VOCAB_SIZE msub -V feedforward.pbs
   done
 done
